ea0b102e50d71423c48feb20e0fc22e26979e759
Nino Chimento <saninoc84@gmil.com>
2021-02-02 23:11:39 +0100

    example call

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+
+  const sendRequest = (): void => {
+    axios.get(`http://localhost/farc/public/farc_users`).then((res) => {
+      setUser(res.data);
+    });
+  };
+
+  console.log(password);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <button onClick={sendRequest} className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+    </>
   );
 }
 
